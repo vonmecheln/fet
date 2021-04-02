00cfb1c61c9f9d200d117e920d351616138275a6
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-04-01 22:59:03 -0300

    check_arguments

--- fet_version.sh
+++ fet_version.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+
+FET_VERSION=""
+
+check_arguments()
+{
+    if [ -z "$1" ]; then
+        echo -e "\nPlease call '$0 <version>' to run this command!\n"
+        exit 1
+    fi
+    FET_VERSION=$1
+}
 
 #receber a versão por parametro
+check_arguments $1
+echo $FET_VERSION
 #baixar o fonte da versão
 #limpar a pasta fet
 #descompactar na pasta fet
 #commitar as alterações
 #criar uma tag
 #enviar para o git
+
+
+
+
