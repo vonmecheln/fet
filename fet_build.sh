e2b322f7528a8c1602e97d13548436c88f40c583
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-09-28 10:25:16 -0300

    build

--- fet_build.sh
+++ fet_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#rm -fr build
+
 mkdir -p build
+mkdir -p build/translations
 cd build
-rm fet*
+rm fet* -rf
 qmake ../fet/fet.pro
 make -j 8
+
+# cp ../fet/fet/translations /usr/share/fet/ -r
+cp ../fet/fet/translations build/ -r
